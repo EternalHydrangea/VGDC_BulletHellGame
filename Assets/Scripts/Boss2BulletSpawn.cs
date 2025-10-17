e49712ff03e9e469890e37e6c6dfe98397ebc04c
bennyshi1 <q1711867267@gmail.com>
2025-10-16 19:31:43 -0500

    something
    lol

--- Assets/Scripts/Boss2BulletSpawn.cs
+++ Assets/Scripts/Boss2BulletSpawn.cs
@@ -1,18 +1,18 @@
-using System.Runtime.CompilerServices;
 using UnityEngine;
-//this file is use to spawn bullets at regular intervals
-public class spawnBulletEnemy : MonoBehaviour
+using System.Runtime.CompilerServices;
 
+public class Boss2BulletSpawn : MonoBehaviour
 {
     public BouncingMovement bulletPrefab;
     public int GameTime = 0;
-//reset the timer when the game starts
+    //reset the timer when the game starts
     void Start()
     {
         GameTime = 0;
     }
-//spawn a bullet at the current position and rotation of the spawner
-    void Spawn(){
+    //spawn a bullet at the current position and rotation of the spawner
+    void Spawn()
+    {
 
         {
             BouncingMovement Bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
@@ -26,20 +26,20 @@ public class spawnBulletEnemy : MonoBehaviour
             Bullet.velocity = new Vector2(4f, 5f);
             Bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
             //up
-            //Bullet.velocity = new Vector2(0f, 2f);
-            //Bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            Bullet.velocity = new Vector2(0f, 2f);
+            Bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
             //down
-            //Bullet.velocity = new Vector2(0f, -2f);
-            //Bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            Bullet.velocity = new Vector2(0f, -2f);
+            Bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
             //left
-            //Bullet.velocity = new Vector2(-2f, 0f);
-            //Bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            Bullet.velocity = new Vector2(-2f, 0f);
+            Bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
             //right
-            //Bullet.velocity = new Vector2(2f, 0f);
-            //Bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            Bullet.velocity = new Vector2(2f, 0f);
+            Bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
             //top right
             Bullet.velocity = new Vector2(-5f, 4f);
-            
+
         }
     }
     // Increment the game time and spawn bullets at regular intervals
@@ -52,5 +52,4 @@ public class spawnBulletEnemy : MonoBehaviour
         }
         //Debug.Log(GameTime);
     }
-    
 }
